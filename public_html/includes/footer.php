13282369f28e72b4c569f9d0161251f78fea1b07
harshit <harshsachdeva80@gmail.com>
2018-01-21 19:50:24 +0530

    Initial Commit

---
+++ public_html/includes/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    </head>
+    <body>
+        <footer>
+            <div class="container">
+                <center>
+                    <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>	
+                </center>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ public_html/index.php
@@ -0,0 +1,85 @@
+
+<html>
+    <head>
+        <title>Welcome | Life Style Store</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <link rel="icon" type="image/png" href="images/logo.png">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css" >
+        <script src="bootstrap/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html">Lifestyle Store</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="signup.html"><span class="glyphicon glyphicon-user "></span> SignUp</a></li>
+                        <li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>     
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <div id="content">
+            <div id="banner_image">
+                <div class="container">
+                    <div id="banner_content">
+                        <h1>We Sell Lifestyle</h1>
+                        <p>Flat 40% OFF on premium brands </p>
+                        <a href="products.html">
+                            <button type="button" class="btn btn-danger btn-lg active ">Shop Now</button>
+                        </a>     
+                    </div>
+                </div>  
+            </div>
+  
+
+            <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="thumbnail">
+                        <img src="images/camera.jpg" alt="camera" class="img-responsive"> 
+                        <div class="caption details">
+                            <h3>Cameras</h3>
+                            <p>Choose among the best available in the world.</p>
+                        </div>
+                     </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="thumbnail"><img src="images/watch.jpg" alt="watch" class="img-responsive">
+                        <div class="caption details">
+                            <h3>Watches</h3> 
+                            <p>Original watches from the best brands</p> 
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="thumbnail"><img src="images/shirt.jpg" alt="shirts" class="img-responsive"> 
+                        <div class="caption details">
+                            <h3>Shirts</h3> 
+                            <p>Our collection of Tshirts</p>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+
+        </div>
+        </div>
+	
+<!--        <footer>
+            <div class="container">
+                <center>Copyright @ Lifestyle Store. All Rights Reserved and Contact Us: +91 90000 00000</center>
+            </div>
+            
+        </footer>-->
+    </body>
+</html>
